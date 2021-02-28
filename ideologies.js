0ac23cdcb5304c0baa987b08ec59d1ca40b9dce4
QUARK88 <72112478+QUARK88@users.noreply.github.com>
2021-02-28 18:56:13 -0500

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,101 @@
+ideologies = [
+    {
+        "name": "Paternalistic Conservatism",
+        "stats": {
+            "econ": 30,
+            "dipl": 75,
+            "govt": 20,
+            "scty": 55
+        }
+    },
+    {
+        "name": "Inhibition",
+        "stats": {
+            "econ": 0,
+            "dipl": 100,
+            "govt": 0,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Libertarian Socialism",
+        "stats": {
+            "econ": 40,
+            "dipl": 0,
+            "govt": 60,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Libertarian Market Socialism",
+        "stats": {
+            "econ": 70,
+            "dipl": 5,
+            "govt": 65,
+            "scty": 15
+        }
+    },
+    {
+        "name": "Anarcho Capitalism",
+        "stats": {
+            "econ": 95,
+            "dipl": 100,
+            "govt": 60,
+            "scty": 0
+        }
+    },
+    {
+        "name": "Market Anarchism",
+        "stats": {
+            "econ": 80,
+            "dipl": 15,
+            "govt": 70,
+            "scty": 0
+        }
+    },
+    {
+        "name": "FALGSC",
+        "stats": {
+            "econ": 70,
+            "dipl": 5,
+            "govt": 95,
+            "scty": 25
+        }
+    },
+    {
+        "name": "Fordism",
+        "stats": {
+            "econ": 90,
+            "dipl": 60,
+            "govt": 100,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Quarkism",
+        "stats": {
+            "econ": 20,
+            "dipl": 40,
+            "govt": 70,
+            "scty": 55
+        }
+    },
+    {
+        "name": "Primitivism",
+        "stats": {
+            "econ": 10,
+            "dipl": 20,
+            "govt": 15,
+            "scty": 10
+        }
+    },
+    {
+        "name": "Capacitationism",
+        "stats": {
+            "econ": 100,
+            "dipl": 0,
+            "govt": 100,
+            "scty": 0
+        }
+    }
+];
